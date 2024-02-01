963e25db41015509f222b34be8b3350066d30b7b
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-02-01 23:21:47 +0100

    add leftover command as parameter

--- iso/build.sh
+++ iso/build.sh
@@ -41,6 +41,8 @@ rm -r extracted
 # amélioré:
 # boot en BIOS et en UEFI
 #sudo mkisofs -o ../alpine-virt-remake.iso -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -J -R -V "Alpine remake" .
+#mkisofs -o ../alpine-virt-remake.iso -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -J -R -V "Alpine remake" .
+
 
 
 # amélioré ?
