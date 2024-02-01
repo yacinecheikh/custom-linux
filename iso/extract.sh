aa8a586c1aa783555c5b7c290f8405cb0eebaf39
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-02-01 23:16:44 +0100

    make iso script pathing absolute; add generic build.sh script

--- iso/extract.sh
+++ iso/extract.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+src=$1
+dest=$2
+
 # clean temp files
-rm -r extracted
+rm -r "$dest"
 
 
 ##### connect the iso and mount it
 
-loopdevice=$(udisksctl loop-setup -f base.iso | cut -d " " -f 5 | cut -d "." -f 1)
+loopdevice=$(udisksctl loop-setup -f "$src" | cut -d " " -f 5 | cut -d "." -f 1)
 
-# wait for device partitions to be detected
+# wait for device partitions to be detected by "while true {check()}"-ing them
 while [ -z "$(udisksctl info --block-device $loopdevice | grep PartitionTable)" ]
 do
 	:
-	#sleep 0
 done
 
 mountpath=$(udisksctl mount --block-device "$loopdevice"p1 | cut -c 25-)
@@ -24,11 +26,11 @@ mountpath=$(udisksctl mount --block-device "$loopdevice"p1 | cut -c 25-)
 
 ##### extract files
 
-cp -r "$mountpath" extracted/
+cp -r "$mountpath" "$dest"
 #echo -"$mountpath"- # debug
 
 # need to add files and then delete the folder
-chmod -R u+rwx extracted
+chmod -R u+rwx "$dest"
 
 
 
--- iso/pack.sh
+++ iso/pack.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+src="$1"
+
+# $dst needs to be absolute because of the `cd` command
+dst_dir="$(dirname "$2")"
+dst_file="$(basename "$2")"
+dst_dir_abs="$(cd "$dst_dir" && pwd)"
+dst="$dst_dir_abs/$dst_file"
+
 
 # package as an iso
-cd extracted
-mkisofs -o ../alpine-virt-remake.iso -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -J -R -V "Alpine remake" .
-cd ..
+cd "$src"
+mkisofs -o "$dst" -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -J -R -V "Custom Linux" .
 
