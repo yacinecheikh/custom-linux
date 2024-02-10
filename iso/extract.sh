347d3097b93fad2c6c3a29e3e74a35e4a1a80b5d
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-02-10 13:37:36 +0100

    verbosify iso extraction script

--- iso/extract.sh
+++ iso/extract.sh
@@ -15,6 +15,7 @@ fi
 ##### connect the iso and mount it
 
 loopdevice=$(udisksctl loop-setup -f "$src" | cut -d " " -f 5 | cut -d "." -f 1)
+echo "created loop device at $loopdevice"
 
 # wait for device partitions to be detected by "while true {check()}"-ing them
 while [ -z "$(udisksctl info --block-device $loopdevice | grep PartitionTable)" ]
@@ -23,6 +24,7 @@ do
 done
 
 mountpath=$(udisksctl mount --block-device "$loopdevice"p1 | cut -c 25-)
+echo "mounted the loop device at $mountpath"
 
 # does this partition have any use ?
 #udisksctl mount --block-device "$loopdevice"p2 | cut -c 25-
@@ -37,11 +39,15 @@ cp -r "$mountpath" "$dest"
 # need to add files and then delete the folder
 chmod -R u+rwx "$dest"
 
+echo "extracted files from $loopdevice"
+
 
 
 ##### disconnect
 
 # unmounting does not require the mount path
 udisksctl unmount --block-device "$loopdevice"p1
+echo "unmounted $mountpath"
 
 udisksctl loop-delete --block-device $loopdevice
+echo "removed $loopdevice loop device"
