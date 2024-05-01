db902b14e38bff2f40367ac9f5985daa81065cf7
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2024-05-01 17:47:25 +0200

    more checks

--- installer-files/checks.py
+++ installer-files/checks.py
@@ -2,9 +2,13 @@ from lib.core import get
 
 
 def check_dependencies():
+    if get("graphics.preset") == "river" and not get("system.repository.apk.edge-testing"):
+        print("riverwm cannot be installed without enabling the APK edge/testing repository")
+        sys.exit()
+
+def check_compat():
     if get("install.mode") == "full" and get("installer.auto"):
         print("full install cannot be completely automated (due to the luks encryption passphrase)")
         sys.exit()
-
 
 # TODO: openssh/other ssh
